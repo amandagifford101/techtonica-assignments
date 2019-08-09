68ab127f29832c16050b1f1b47d854eed0187b57
Amanda Gifford <amandagifford@Amandas-MacBook-Pro.local>
2019-08-09 11:33:39 -0700

    miniproject working

---
+++ Projects/event.js
@@ -0,0 +1,60 @@
+class Ticket {
+    constructor(type, cost) {
+        this.ticketType = type;
+        this.cost = cost;
+    }
+}
+
+class Event {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+        this.availableTickets =[]
+        }
+        addAvailableTickets(ticketType, ticketCost) {
+            const newTicket = new Ticket(ticketType, ticketCost);
+            this.availableTickets.push(newTicket);
+        }
+        searchTickets(minPrice, maxPrice) { 
+            let message = "Eligible tickets: ";
+            let counter = 1;
+            for (let i=0; i<this.availableTickets.length; i++) {
+                if (this.availableTickets[i].cost >= minPrice && this.availableTickets[i].cost <= maxPrice) {
+                    message += counter + ". " + this.availableTickets[i].ticketType + " ";
+                    counter++;
+                }
+            }
+            if (counter === 1){
+                message = "There are no available tickets";
+            }
+            return message;
+        }
+    }
+
+
+const event_obj1 = new Event("Black Keys Concert", "Win a guitar signed by the entire band!");
+const event_obj2 = new Event("Zeppelin Constituents: A Led Zeppelin Tribute Band", "Come boogie down to your favorite classics!");
+const event_obj3 = new Event("Gala for Harvey Milk Foundation", "Come join us for live music, food, and rainbow vibes!");
+
+event_obj1.addAvailableTickets("General Admission", 80);
+event_obj2.addAvailableTickets("General Admission", 33);
+event_obj3.addAvailableTickets("Balcony", 99);
+event_obj1.addAvailableTickets("VIP", 202);
+event_obj2.addAvailableTickets("VIP", 303);
+event_obj3.addAvailableTickets("VIP", 404);
+
+var event_array = new Array();
+
+event_array.push(event_obj1, event_obj2, event_obj3);
+
+console.log(event_array);
+
+
+$(document).ready(function() {
+    let html = "";
+    $.each(event_array, function(index, item) {
+    html+= `<li>${item.name} - ${item.description} - ${item.searchTickets(0, 100)}</li>`;
+    });
+    // insert final html into #event...
+    $("#event").html(html);
+});
