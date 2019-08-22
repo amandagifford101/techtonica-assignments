99bc919547d9a7ebdc68d9f2e13bcd4baa1d476c
Amanda Gifford <amandagifford@Amandas-MacBook-Pro.local>
2019-08-22 13:51:54 -0700

    Everything is working correctly

---
+++ Projects/node-api-postgres/queries.js
@@ -0,0 +1,79 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'kaygifford101',
+  host: 'localhost',
+  database: 'api',
+  password: 'Ttouwa101339933',
+  port: 5432,
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+//GET request
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+//GET request
+
+  const createUser = (request, response) => {
+    const { name, email } = request.body
+  
+    pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, result) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User added with ID: ${result.insertId}`)
+    })
+  }
+  //POST request
+
+  const updateUser = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { name, email } = request.body
+  
+    pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+      [name, email, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+      }
+    )
+  }
+//PUT requests are idempotent, exact same call can be made over and over and produce same result
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User deleted with ID: ${id}`)
+    })
+  }
+  //DELETE request
+
+  module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+  }
